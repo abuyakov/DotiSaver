382454a6c6bb2194d77809029c1eb6746f1fb815
abuyakov <alexander.buyakov@gmail.com>
2023-03-03 18:35:18 +0700

    Initial Commit

---
+++ DotSaver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DotSaver
+//
+//  Created by Aleksandr Buiakov on 03.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
